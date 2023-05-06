695a5b1fd21a520706f472a25113b21bea42d6fe
mohamed uzman <68750653+uzman474@users.noreply.github.com>
2023-05-06 12:36:39 +0530

    main
    main

---
+++ app/src/main/java/com/example/pkart/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pkart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.pkart.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    var i = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+
+        binding.bottomBar.onItemSelected = {
+            when (it) {
+                0 -> {
+                    i = 0;
+                    navController.navigate(R.id.homeFragment)
+                }
+                1 -> i = 1
+                2 -> i = 2
+            }
+        }
+    }
+        override fun onBackPressed(){
+            super.onBackPressed()
+            if(i == 0){
+                finish()
+            }
+
+
+    }
+}
